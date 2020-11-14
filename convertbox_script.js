0921edf33379c6984f00d1db84c3c0956f51ddb9
ttngu <tammyn137@gmail.com>
2020-11-13 17:50:42 -0800

    button console logs

---
+++ convertbox_script.js
@@ -0,0 +1,17 @@
+// Define variable for unit input
+var input = $(".number-input");
+// // // Define variable for unit output
+// var output = $(".output");
+
+// When a number is entered to the input
+// Then a unit input must be selected
+
+// Then a unit output must be selected
+// On click, the convert button will convert the input value to the output value
+$(".convert-btn").click(function(){
+    console.log("The convert button was clicked.");
+
+    $(input).val();
+    console.log(input)
+
+  });
