5b9eb864e419823a0a7d0474965a1dfacf4ae800
ttngu <tammyn137@gmail.com>
2020-11-14 17:14:16 -0800

    input box takes in value and units

--- convertbox_script.js
+++ convertbox_script.js
@@ -1,17 +1,31 @@
-// Define variable for unit input
-var input = $(".number-input");
-// // // Define variable for unit output
-// var output = $(".output");
+var inputUnit;
 
 // When a number is entered to the input
 // Then a unit input must be selected
 
 // Then a unit output must be selected
 // On click, the convert button will convert the input value to the output value
+
+$(".input-unit").click(function(){
+  inputUnit = $(this).attr("id");
+  console.log(inputUnit)
+})
+
+
 $(".convert-btn").click(function(){
-    console.log("The convert button was clicked.");
+ 
+  console.log("The convert button was clicked.");
 
-    $(input).val();
-    console.log(input)
+  // Define variable for unit input
+  var input = JSON.parse(document.getElementById("input-value").value);
+  // // // Define variable for unit output
+  var output = $(".output").value;
+  console.log(input);
 
-  });
+  
+});
+
+
+
+
+
