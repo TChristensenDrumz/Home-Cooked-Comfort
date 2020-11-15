0b27be0227d56fa7ada94039e87c90c6a2a0c07b
ttngu <tammyn137@gmail.com>
2020-11-14 17:56:17 -0800

    convert box buttons all function

--- convertbox_script.js
+++ convertbox_script.js
@@ -1,4 +1,6 @@
 var inputUnit;
+var outputUnit;
+let resultValue = 0;
 
 // When a number is entered to the input
 // Then a unit input must be selected
@@ -11,17 +13,33 @@ $(".input-unit").click(function(){
   console.log(inputUnit)
 })
 
+$(".output-unit").click(function(){
+  outputUnit = $(this).attr("id");
+  console.log(outputUnit)
+})
 
 $(".convert-btn").click(function(){
  
   console.log("The convert button was clicked.");
 
   // Define variable for unit input
-  var input = JSON.parse(document.getElementById("input-value").value);
+  var inputValue = JSON.parse(document.getElementById("input-value").value);
   // // // Define variable for unit output
-  var output = $(".output").value;
-  console.log(input);
+  var outputValue = document.getElementById("output-value")
 
+
+  // Conversion table
+  if (inputUnit == "cups" && outputUnit == "cups") resultValue = inputValue;
+  console.log(resultValue);
+
+  if (inputUnit == "cups" && outputUnit == "tbsp") resultValue = inputValue * 16;
+  console.log(resultValue)
+  if (inputUnit == "cups" && outputUnit == "oz") outputValue = 0.393701;
+
+
+
+  // Show output in html text div
+  outputValue.innerHTML = resultValue;
   
 });
 
